0bde5b32c82cffb28e496d77d482f9ef1b46fc49
王大宸 <240216240@qq.com>
2023-04-10 15:57:01 +0800

    版本修改

---
+++ fly-context/src/main/java/com/github/itdachen/framework/context/annotation/CurrentUser.java
@@ -0,0 +1,16 @@
+package com.github.itdachen.framework.context.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Description:  在 Controller 的方法参数中使用此注解，该方法在映射时会注入当前登录的 User对象
+ * Created by 王大宸 on 2023/04/10 10:19
+ * Created with IntelliJ IDEA.
+ */
+@Target(ElementType.PARAMETER)// 可用在方法的参数上
+@Retention(RetentionPolicy.RUNTIME)// 运行时有效
+public @interface CurrentUser {
+}
