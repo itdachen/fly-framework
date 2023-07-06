e6b878c42515bbcbf36b70da8858592f83d9b35b
王大宸 <240216240@qq.com>
2023-07-06 14:50:19 +0800

    阿里 连接池更改

--- fly-datasource/src/main/java/com/github/itdachen/framework/datasource/config/DruidDataSourceConfiguration.java
+++ fly-datasource/src/main/java/com/github/itdachen/framework/datasource/config/DruidDataSourceConfiguration.java
@@ -1,7 +1,10 @@
 package com.github.itdachen.framework.datasource.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.jakarta.StatViewServlet;
+import com.alibaba.druid.support.jakarta.WebStatFilter;
 import com.github.itdachen.framework.datasource.encoder.DataSourceEncoder;
+import jakarta.servlet.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -9,8 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * Description: 阿里巴巴开源数据源自定义配置,连接地址可以加密
@@ -26,8 +35,6 @@ public class DruidDataSourceConfiguration extends DruidDataSource implements Ini
 
     @Autowired
     private DataSourceEncoder dataSourceEncoder;
-//    @Autowired
-//    private DataSourceProperties basicProperties;
 
     @Value("${spring.datasource.driver-class-name}")
     private String driverClassName;
@@ -64,5 +71,93 @@ public class DruidDataSourceConfiguration extends DruidDataSource implements Ini
             super.setDriverClassName(str);
         }
     }
+
+
+    /***
+     * 注册Servlet信息，配置数据库监控视图
+     *
+     * @author 王大宸
+     * @date 2020/10/11 15:32
+     * @return org.springframework.boot.web.servlet.ServletRegistrationBean<com.alibaba.druid.support.http.StatViewServlet>
+     */
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidServlet() {
+        logger.info("正在注册Servlet信息，配置数据库监控视图...");
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<StatViewServlet>();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        logger.info("数据库数据库监控视图配置完成");
+        return servletRegistrationBean;
+    }
+
+    /***
+     * 注册Filter信息, 数据库监控拦截器
+     *
+     * @author 王大宸
+     * @date 2020/10/11 15:32
+     * @return org.springframework.boot.web.servlet.FilterRegistrationBean<com.alibaba.druid.support.http.WebStatFilter>
+     */
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> filterRegistrationBean() {
+        logger.info("正在注册Filter信息, 数据库监控拦截器...");
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<WebStatFilter>();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
+        filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
+        filterRegistrationBean.addInitParameter("DruidWebStatFilter", "/*");
+        logger.info("数据库监控拦截器配置完成");
+        return filterRegistrationBean;
+    }
+
+    /**
+     * 去除广告
+     *
+     * @param properties
+     * @return
+     */
+//    @SuppressWarnings({"rawtypes", "unchecked"})
+//    @Bean
+//    @ConditionalOnProperty(name = "spring.datasource.druid.stat-view-servlet.enabled", havingValue = "true", matchIfMissing = true)
+//    public FilterRegistrationBean removeDruidAdFilterRegistrationBean(DruidStatProperties properties) {
+//        // 获取web监控页面的参数
+//        DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
+//        // 提取common.js的配置路径
+//        String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
+//        String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
+//
+//        final String filePath = "support/http/resources/js/common.js";
+//
+//        // 创建filter进行过滤
+//        Filter filter = new Filter() {
+//            @Override
+//            public void init(FilterConfig filterConfig) throws ServletException {
+//            }
+//
+//            @Override
+//            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//                    throws IOException, ServletException {
+//                chain.doFilter(request, response);
+//                // 重置缓冲区，响应头不会被重置
+//                response.resetBuffer();
+//                // 获取common.js
+//                String text = Utils.readFromResource(filePath);
+//                // 正则替换banner, 除去底部的广告信息
+//                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
+//                text = text.replaceAll("powered.*?shrek.wang</a>", "");
+//                response.getWriter().write(text);
+//            }
+//
+//            @Override
+//            public void destroy() {
+//            }
+//        };
+//        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+//        registrationBean.setFilter(filter);
+//        registrationBean.addUrlPatterns(commonJsPattern);
+//        return registrationBean;
+//    }
 
 }
--- fly-druid-view/src/main/java/com/github/itdachen/framework/datasource/druid/view/DruidDataSourceStatViewServletView.java
+++ fly-druid-view/src/main/java/com/github/itdachen/framework/datasource/druid/view/DruidDataSourceStatViewServletView.java
@@ -1,19 +1,13 @@
 package com.github.itdachen.framework.datasource.druid.view;
 
-import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-import com.alibaba.druid.util.Utils;
-import jakarta.servlet.*;
+import com.alibaba.druid.support.jakarta.StatViewServlet;
+import com.alibaba.druid.support.jakarta.WebStatFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
 
 /**
  * Description: Alibaba Druid 连接池可视化监控
@@ -21,6 +15,7 @@ import java.io.IOException;
  * Created with IntelliJ IDEA.
  */
 @Configuration
+@Deprecated
 public class DruidDataSourceStatViewServletView {
     private static final Logger logger = LoggerFactory.getLogger(DruidDataSourceStatViewServletView.class);
 
@@ -31,15 +26,15 @@ public class DruidDataSourceStatViewServletView {
      * @date 2020/10/11 15:32
      * @return org.springframework.boot.web.servlet.ServletRegistrationBean<com.alibaba.druid.support.http.StatViewServlet>
      */
-//    @Bean
-//    public ServletRegistrationBean<StatViewServlet> druidServlet() {
-//        logger.info("正在注册Servlet信息，配置数据库监控视图...");
-//        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<StatViewServlet>();
-//        servletRegistrationBean.setServlet(new StatViewServlet());
-//        servletRegistrationBean.addUrlMappings("/druid/*");
-//        logger.info("数据库数据库监控视图配置完成");
-//        return servletRegistrationBean;
-//    }
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidServlet() {
+        logger.info("正在注册Servlet信息，配置数据库监控视图...");
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<StatViewServlet>();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        logger.info("数据库数据库监控视图配置完成");
+        return servletRegistrationBean;
+    }
 
     /***
      * 注册Filter信息, 数据库监控拦截器
@@ -48,20 +43,20 @@ public class DruidDataSourceStatViewServletView {
      * @date 2020/10/11 15:32
      * @return org.springframework.boot.web.servlet.FilterRegistrationBean<com.alibaba.druid.support.http.WebStatFilter>
      */
-//    @Bean
-//    public FilterRegistrationBean<WebStatFilter> filterRegistrationBean() {
-//        logger.info("正在注册Filter信息, 数据库监控拦截器...");
-//        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<WebStatFilter>();
-//        filterRegistrationBean.setFilter(new WebStatFilter());
-//        filterRegistrationBean.addUrlPatterns("/*");
-//        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
-//        filterRegistrationBean.addInitParameter("profileEnable", "true");
-//        filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
-//        filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
-//        filterRegistrationBean.addInitParameter("DruidWebStatFilter", "/*");
-//        logger.info("数据库监控拦截器配置完成");
-//        return filterRegistrationBean;
-//    }
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> filterRegistrationBean() {
+        logger.info("正在注册Filter信息, 数据库监控拦截器...");
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<WebStatFilter>();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
+        filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
+        filterRegistrationBean.addInitParameter("DruidWebStatFilter", "/*");
+        logger.info("数据库监控拦截器配置完成");
+        return filterRegistrationBean;
+    }
 
     /**
      * 去除广告
@@ -69,46 +64,45 @@ public class DruidDataSourceStatViewServletView {
      * @param properties
      * @return
      */
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    @Bean
-    @ConditionalOnProperty(name = "spring.datasource.druid.stat-view-servlet.enabled", havingValue = "true", matchIfMissing = true)
-    public FilterRegistrationBean removeDruidAdFilterRegistrationBean(DruidStatProperties properties) {
-        // 获取web监控页面的参数
-        DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
-        // 提取common.js的配置路径
-        String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
-        String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
-
-        final String filePath = "support/http/resources/js/common.js";
-
-        // 创建filter进行过滤
-        Filter filter = new Filter() {
-            @Override
-            public void init(FilterConfig filterConfig) throws ServletException {
-            }
-
-            @Override
-            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-                    throws IOException, ServletException {
-                chain.doFilter(request, response);
-                // 重置缓冲区，响应头不会被重置
-                response.resetBuffer();
-                // 获取common.js
-                String text = Utils.readFromResource(filePath);
-                // 正则替换banner, 除去底部的广告信息
-                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
-                text = text.replaceAll("powered.*?shrek.wang</a>", "");
-                response.getWriter().write(text);
-            }
-
-            @Override
-            public void destroy() {
-            }
-        };
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(filter);
-        registrationBean.addUrlPatterns(commonJsPattern);
-        return registrationBean;
-    }
+//    @SuppressWarnings({"rawtypes", "unchecked"})
+//    @Bean
+//    public FilterRegistrationBean removeDruidAdFilterRegistrationBean(DruidStatProperties properties) {
+//        // 获取web监控页面的参数
+//        DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
+//        // 提取common.js的配置路径
+//        String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
+//        String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
+//
+//        final String filePath = "support/http/resources/js/common.js";
+//
+//        // 创建filter进行过滤
+//        Filter filter = new Filter() {
+//            @Override
+//            public void init(FilterConfig filterConfig) throws ServletException {
+//            }
+//
+//            @Override
+//            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//                    throws IOException, ServletException {
+//                chain.doFilter(request, response);
+//                // 重置缓冲区，响应头不会被重置
+//                response.resetBuffer();
+//                // 获取common.js
+//                String text = Utils.readFromResource(filePath);
+//                // 正则替换banner, 除去底部的广告信息
+//                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
+//                text = text.replaceAll("powered.*?shrek.wang</a>", "");
+//                response.getWriter().write(text);
+//            }
+//
+//            @Override
+//            public void destroy() {
+//            }
+//        };
+//        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+//        registrationBean.setFilter(filter);
+//        registrationBean.addUrlPatterns(commonJsPattern);
+//        return registrationBean;
+//    }
 
 }
