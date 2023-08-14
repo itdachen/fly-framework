1cda39cece9114e16520a6b9a60aecd43340dfaf
王大宸 <240216240@qq.com>
2023-08-14 22:58:46 +0800

    1.0.1

--- fly-log/src/main/java/com/github/itdachen/framework/log/aspectj/LogAspectj.java
+++ fly-log/src/main/java/com/github/itdachen/framework/log/aspectj/LogAspectj.java
@@ -13,7 +13,6 @@ import com.github.itdachen.framework.threads.manager.AsyncThreadsManager;
 import com.github.itdachen.framework.tools.ServletUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
@@ -23,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 
 /**
@@ -36,9 +34,8 @@ import java.time.LocalDateTime;
 public class LogAspectj {
     private static final Logger logger = LoggerFactory.getLogger(LogAspectj.class);
 
-    // 配置织入点
-    @Pointcut("@annotation(com.github.itdachen.framework.context.annotation.Log)")
-    public void logPointCut() {
+    @Pointcut("@annotation(log)")
+    public void logPointCut(Log log) {
     }
 
     /**
@@ -46,79 +43,89 @@ public class LogAspectj {
      *
      * @param joinPoint 切点
      */
-    @AfterReturning(pointcut = "logPointCut()", returning = "jsonResult")
-    public void doAfterReturning(JoinPoint joinPoint, Object jsonResult) {
-        handleLog(joinPoint, null, jsonResult);
-    }
+    @AfterReturning(pointcut = "logPointCut(log)", returning = "resJson", argNames = "joinPoint,log,resJson")
+    public void doAfterReturning(JoinPoint joinPoint, Log log, Object resJson) {
+        if (null == log) {
+            return;
+        }
+        // *========数据库日志=========*//
+        ApiLogClient apiLog = new ApiLogClient();
+        setRequest(apiLog, joinPoint, log);
 
-    /**
-     * 拦截异常操作
-     *
-     * @param joinPoint 切点
-     * @param e         异常
-     */
-    @AfterThrowing(value = "logPointCut()", throwing = "e")
-    public void doAfterThrowing(JoinPoint joinPoint, Exception e) {
-        handleLog(joinPoint, e, null);
-    }
+        JSONObject json = (JSONObject) JSON.toJSON(resJson);
+        apiLog.setMakeUseStatus(ApiLogConstant.IS_OK);
+        apiLog.setMsg(json.getString("msg"));
+        apiLog.setJsonResult(JSONObject.toJSONString(resJson));
 
-    /**
-     * @param joinPoint  切点
-     * @param e          异常
-     * @param resultJson 返回数据
-     */
-    protected void handleLog(final JoinPoint joinPoint, final Exception e, Object resultJson) {
+        if (!json.getBooleanValue("success")) {
+            apiLog.setMakeUseStatus(ApiLogConstant.IS_ERR);
+        }
+
         try {
-            // 获得注解
-            Log log = getAnnotationLog(joinPoint);
-            if (null == log) {
-                return;
-            }
-
-            // *========数据库日志=========*//
-            ApiLogClient apiLog = new ApiLogClient();
-            apiLog.setRequestMethod(ServletUtils.getRequest().getMethod());
-            setRequestValue(apiLog, joinPoint);
-            apiLog.setTenantId(BizContextHandler.getTenantId());
-            apiLog.setJsonResult(JSONObject.toJSONString(resultJson));
-            apiLog.setServiceId(ServletUtils.getRequest().getContextPath());
-            apiLog.setRequestUri(ServletUtils.getRequest().getRequestURI());
-            apiLog.setMakeUseIp(ServletUtils.getIPAddress());
-            apiLog.setLogType(log.type());
-            apiLog.setMakeUseType(log.title());
-//            String className = joinPoint.getTarget().getClass().getName();
-//            String methodName = joinPoint.getSignature().getName();
-            //  apiLog.set(className + "." + methodName + "()");
-
-            // 获取当前的用户
-            apiLog.setId(IdUtils.getId());
-            apiLog.setCreateTime(LocalDateTime.now());
-            apiLog.setCreateUser(BizContextHandler.getNickName());
-            apiLog.setCreateUserId(BizContextHandler.getUserId());
-
-            CheckApiClient apiClient = joinPoint.getTarget().getClass().getAnnotation(CheckApiClient.class);
-            if (null != apiClient) {
-                apiLog.setMenuTitle(apiClient.title());
-                apiLog.setClientId(apiClient.clientId());
-            }
-
-            JSONObject res = (JSONObject)JSON.toJSON(resultJson);
-
-            // 业务操作
-         //   ServerResponse res = ObjectUtils.objToClass(resultJson, ServerResponse.class);
-            if (null != res) {
-                apiLog.setMakeUseStatus(ApiLogConstant.IS_OK);
-                apiLog.setMsg(res.getString("msg"));
-                if (!res.getBooleanValue("success")) {
-                    apiLog.setMakeUseStatus(ApiLogConstant.IS_ERR);
-                }
-            }
             // 保存数据库
             AsyncThreadsManager.me().execute(AsyncFactory.recordOper(apiLog));
         } catch (Exception exp) {
             // 记录本地异常日志
             logger.error("==前置通知异常==异常信息: {}", exp.getMessage(), exp);
         }
+    }
+
+    /**
+     * 拦截异常操作
+     *
+     * @param joinPoint 切点
+     * @param ex        异常
+     */
+    @AfterThrowing(value = "logPointCut(log)", throwing = "ex", argNames = "joinPoint,log,ex")
+    public void doAfterThrowing(JoinPoint joinPoint, Log log, Exception ex) {
+        if (null == log) {
+            return;
+        }
+        // *========数据库日志=========*//
+        ApiLogClient apiLog = new ApiLogClient();
+
+        setRequest(apiLog, joinPoint, log);
+
+        /* 响应数据 */
+        JSONObject json = (JSONObject) JSON.toJSON(ex);
+
+        apiLog.setJsonResult(getMessage(json.getIntValue("status"), ex.getMessage()));
+        apiLog.setMakeUseStatus(ApiLogConstant.IS_ERR);
+        apiLog.setMsg(json.getString("msg"));
+
+        try {
+            // 保存数据库
+            AsyncThreadsManager.me().execute(AsyncFactory.recordOper(apiLog));
+        } catch (Exception exp) {
+            // 记录本地异常日志
+            logger.error("==前置通知异常==异常信息: {}", exp.getMessage(), exp);
+        }
+
+    }
+
+
+    private void setRequest(ApiLogClient apiLog, JoinPoint joinPoint, Log log) {
+        apiLog.setRequestMethod(ServletUtils.getRequest().getMethod());
+        setRequestValue(apiLog, joinPoint);
+        apiLog.setTenantId(BizContextHandler.getTenantId());
+
+        apiLog.setServiceId(ServletUtils.getRequest().getContextPath());
+        apiLog.setRequestUri(ServletUtils.getRequest().getRequestURI());
+        apiLog.setMakeUseIp(ServletUtils.getIPAddress());
+        apiLog.setLogType(log.type());
+        apiLog.setMakeUseType(log.title());
+
+        CheckApiClient apiClient = joinPoint.getTarget().getClass().getAnnotation(CheckApiClient.class);
+        if (null != apiClient) {
+            apiLog.setMenuTitle(apiClient.title());
+            apiLog.setClientId(apiClient.clientId());
+        }
+
+        // 获取当前的用户
+        apiLog.setId(IdUtils.getId());
+        apiLog.setCreateTime(LocalDateTime.now());
+        apiLog.setCreateUser(BizContextHandler.getNickName());
+        apiLog.setCreateUserId(BizContextHandler.getUserId());
     }
 
     /**
@@ -139,17 +146,9 @@ public class LogAspectj {
         apiLog.setParams(StringUtils.substring(jObject.toJSONString(), 0, 2000));
     }
 
-    /**
-     * 是否存在注解，如果存在就获取
-     */
-    private Log getAnnotationLog(JoinPoint joinPoint) {
-        Signature signature = joinPoint.getSignature();
-        MethodSignature methodSignature = (MethodSignature) signature;
-        Method method = methodSignature.getMethod();
-        if (method != null) {
-            return method.getAnnotation(Log.class);
-        }
-        return null;
+    private String getMessage(int status, String msg) {
+        return "{\"success\":\"false\",\"status\":\"" + status + "\", \"msg\":\"" + msg + "\",\"data\": null}";
     }
+
 
 }
