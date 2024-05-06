637417d1c8a7dfc45a292dcc25d95ecb639f989b
王大宸 <240216240@qq.com>
2024-05-06 23:58:28 +0800

    安全认证, 添加自定义鉴权模式

---
+++ fly-context/src/main/java/com/github/itdachen/framework/context/constants/DeptLevelConstants.java
@@ -0,0 +1,42 @@
+package com.github.itdachen.framework.context.constants;
+
+/**
+ * 部门等级
+ * 00-总部/10-省级/20-市州级/30-区县级/40-乡镇/街道级
+ *
+ * @author 王大宸
+ * @date 2024/5/6 23:03
+ */
+public class DeptLevelConstants {
+
+    /**
+     * 总部
+     */
+    public static final String ROOT_LEVEL = "00";
+
+    /**
+     * 省级
+     */
+    public static final String PROV_LEVEL = "10";
+
+    /**
+     * 市/州级
+     */
+    public static final String CITY_LEVEL = "20";
+
+    /**
+     * 区/县级
+     */
+    public static final String COUNT_LEVEL = "30";
+
+    /**
+     * 乡镇/街道级
+     */
+    public static final String STREET_LEVEL = "40";
+
+    /**
+     * 村/社区级
+     */
+    public static final String VILLAGE_LEVEL = "50";
+
+}
