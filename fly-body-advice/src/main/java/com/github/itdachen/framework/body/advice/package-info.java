0ee7323356e4a54a3eb065238d7bb2bbf769678a
王大宸 <240216240@qq.com>
2024-12-01 00:53:10 +0800

    删除不需要的模块

--- fly-body-advice/src/main/java/com/github/itdachen/framework/body/advice/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Description: 统一返回数据格式模块
- * Created by 王大宸 on 2023/03/18 18:29
- * Created with IntelliJ IDEA.
- */
-package com.github.itdachen.framework.body.advice;
---
+++ wechat-spring-boot-starter/src/main/java/com/github/itdachen/boot/wechat/WechatAppletAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.github.itdachen.boot.wechat;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 微信小程序配置
+ *
+ * @author 王大宸
+ * @date 2024/12/1 0:24
+ */
+@Configuration
+public class WechatAppletAutoConfiguration {
+}
