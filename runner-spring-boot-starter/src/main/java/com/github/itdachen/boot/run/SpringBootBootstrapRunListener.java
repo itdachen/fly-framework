7d9e1488e862168561a0f740122c84c2981bc26b
王大宸 <240216240@qq.com>
2025-02-16 22:48:38 +0800

    1.1.0-RC

--- runner-spring-boot-starter/src/main/java/com/github/itdachen/boot/run/SpringBootBootstrapRunListener.java
+++ runner-spring-boot-starter/src/main/java/com/github/itdachen/boot/run/SpringBootBootstrapRunListener.java
@@ -16,6 +16,7 @@ import java.net.UnknownHostException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 /**
  * 自定义启动日志模块
@@ -45,7 +46,7 @@ public class SpringBootBootstrapRunListener implements SpringApplicationRunListe
      */
     @Override
     public void starting(ConfigurableBootstrapContext bootstrapContext) {
-        System.out.println("spring boot bootstrap starting");
+        //   System.out.println("spring boot bootstrap starting");
         application.setBannerMode(Banner.Mode.OFF); // 禁用 Banner
 
         /* 启动及时, 用不到, 保留方法, 以后可能会用到该方法 */
@@ -66,9 +67,13 @@ public class SpringBootBootstrapRunListener implements SpringApplicationRunListe
      */
     @Override
     public void environmentPrepared(ConfigurableBootstrapContext bootstrapContext, ConfigurableEnvironment environment) {
+        String appName = environment.getProperty("spring.application.name");
+        if (null == appName || "".equals(appName) || "null".equals(appName)) {
+            return;
+        }
         final String runLog = SeparatorLine.SEPARATOR_LINE + "\n" +
-                "\twelcome use [ " + environment.getProperty("spring.application.name") + " ] application\n" +
-                "\tapplication [ " + environment.getProperty("spring.application.name") + " ] starting\n" +
+                "\twelcome use [ " + appName + " ] application\n" +
+                "\tapplication [ " + appName + " ] starting\n" +
                 "\tstarting time " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "\n" +
                 "\tframework from https://github.com/itdachen \n" +
                 SeparatorLine.SEPARATOR_LINE;
@@ -127,26 +132,28 @@ public class SpringBootBootstrapRunListener implements SpringApplicationRunListe
      */
     @Override
     public void ready(ConfigurableApplicationContext context, Duration timeTaken) {
+        ConfigurableEnvironment environment = context.getEnvironment();
+        final String contextPath = environment.getProperty("server.servlet.context-path") == null ? "" : environment.getProperty("server.servlet.context-path");
+        if (null == contextPath || "".equals(contextPath) || "null".equals(contextPath)) {
+            return;
+        }
+
         /* 启动耗时, 单位: 秒 */
         long seconds = timeTaken.getSeconds();
         //  logger.info("spring boot application successfully started");
-        ConfigurableEnvironment environment = context.getEnvironment();
+
         final String http = environment.getProperty("server.ssl.key-store") != null ? "https" : "http";
         final String port = environment.getProperty("server.port");
-        final String contextPath = environment.getProperty("server.servlet.context-path") == null ? "" : environment.getProperty("server.servlet.context-path");
+        final String activeProfiles = environment.getActiveProfiles().length == 0 ? "[ bootstrap | application | nacos ]" : Arrays.toString(environment.getActiveProfiles());
+        final String serverHttp = http + "://" + getHostIp() + ":" + port + contextPath;
+        String text = SeparatorLine.SEPARATOR_LINE + "\n\t" +
+                "application [ " + environment.getProperty("spring.application.name") + " ] successfully started \n\t" +
+                "time consuming: \t" + seconds + " s \n\t" +
+                "profiles: \t\t\t" + activeProfiles + "\n\t" +
+                "thread: \t\t\t" + ManagementFactory.getRuntimeMXBean().getPid() + "\n\t" +
+                "request address: \t" + serverHttp + "\n" + SeparatorLine.SEPARATOR_LINE;
 
-        logger.info("\n" + SeparatorLine.SEPARATOR_LINE + "\n\t" +
-                        "application [ {} ] successfully started \n\t" +
-                        "time consuming: \t{} s \n\t" +
-                        "profiles: \t\t\t{}\n\t" +
-                        "thread: \t\t\t{}\n\t" +
-                        "request address: \t{}\n" + SeparatorLine.SEPARATOR_LINE,
-                environment.getProperty("spring.application.name"),
-                seconds,
-                environment.getActiveProfiles().length == 0 ? "[ bootstrap | application | nacos ]" : environment.getActiveProfiles(),
-                ManagementFactory.getRuntimeMXBean().getPid(),
-                http + "://" + getHostIp() + ":" + port + contextPath
-        );
+        System.out.println(text);
     }
 
     /***
