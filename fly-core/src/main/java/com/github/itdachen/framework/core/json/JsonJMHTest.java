655786c63397bb628de63cf27c20cb34c4e593b6
王大宸 <240216240@qq.com>
2025-05-06 14:36:26 +0800

    自定义权限校验方式修改11111

---
+++ fly-core/src/main/java/com/github/itdachen/framework/core/json/JsonJMHTest.java
@@ -0,0 +1,27 @@
+package com.github.itdachen.framework.core.json;
+
+/**
+ * JsonJMHTest 测试
+ *
+ * @author 王大宸
+ * @date 2025-05-06 14:30
+ */
+public class JsonJMHTest {
+
+    private final static String json = "{\"id\":122345667,\"email\":\"jianzh5@163.com\",\"price\":12.25}";
+    //  private final static  UserEntity userEntity = new UserEntity(13345L, "jianzh5@163.com", BigDecimal.valueOf(12.25));
+
+
+//    public UserEntity singleten2ObjTest() throws JsonProcessingException {
+//        ObjectMapper objectMapper = ObjectMapperInstance.INSTANCE.getObjectMapper();
+//        return objectMapper.readValue(json, UserEntity.class);
+//    }
+
+
+//    public String singleten2StringTest() throws JsonProcessingException {
+//        ObjectMapper objectMapper = ObjectMapperInstance.INSTANCE.getObjectMapper();
+//        return objectMapper.writeValueAsString(userEntity);
+//    }
+
+
+}
---
+++ fly-core/src/main/java/com/github/itdachen/framework/core/json/ObjectMapperInstance.java
@@ -0,0 +1,51 @@
+package com.github.itdachen.framework.core.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 创建工具类时要将工具类设置成单例的，
+ * 这样不仅可以保证线程安全，也可以保证在系统全局只能创建一个对象，避免频繁创建对象的成本
+ *
+ * @author 王大宸
+ * @date 2025-05-06 14:28
+ */
+public enum ObjectMapperInstance {
+
+
+    INSTANCE;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    ObjectMapperInstance() {
+
+    }
+
+    public ObjectMapper getObjectMapper() {
+        return objectMapper;
+    }
+
+
+    /****** 个性化单例配置 **********************************************************************************/
+
+    //    INSTANCE;
+//
+//    private final ObjectMapper objectMapper;
+//
+//    ObjectMapperInstance() {
+//        objectMapper = new ObjectMapper();
+//        // 注册自定义模块
+//        initialize();
+//    }
+//
+//    private void initialize() {
+//        CustomJsonModule customJsonModule = new CustomJsonModule();
+//        objectMapper.registerModule(customJsonModule);
+//    }
+//
+//
+//    public ObjectMapper getObjectMapper() {
+//        return objectMapper;
+//    }
+
+
+}
