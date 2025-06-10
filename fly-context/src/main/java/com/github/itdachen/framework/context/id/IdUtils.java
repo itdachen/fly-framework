34f000d2f7204b9e2ad929308ac317cf018c3309
王大宸 <240216240@qq.com>
2025-06-10 16:43:28 +0800

    自定义权限校验方式修改

--- fly-context/src/main/java/com/github/itdachen/framework/context/id/IdUtils.java
+++ fly-context/src/main/java/com/github/itdachen/framework/context/id/IdUtils.java
@@ -4,6 +4,8 @@ import com.github.itdachen.framework.context.id.sharding.SnowflakeShardingKeyGen
 import com.github.itdachen.framework.context.id.utils.GuidUtils;
 import com.github.itdachen.framework.context.id.utils.SnowflakeUtils;
 
+import java.util.Random;
+
 /**
  * Description: ID 生成工具, 对外调用
  * Created by 王大宸 on 2023/01/05 9:33
@@ -12,6 +14,26 @@ import com.github.itdachen.framework.context.id.utils.SnowflakeUtils;
 public class IdUtils {
     private static final SnowflakeShardingKeyGeneratorUtils shardingKeyGenerator = new SnowflakeShardingKeyGeneratorUtils();
     private static final SnowflakeUtils SNOWFLAKE_UTILS = new SnowflakeUtils(1, 1, 0L);
+    private static final String ALL_CHAR_NUM = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    /***
+     * 获取随机长度的串
+     *
+     * @author 王大宸
+     * @date 2025/6/10 16:42
+     * @param length length
+     * @return java.lang.String
+     */
+    public static String getStringNumRandom(int length) {
+        //生成随机数字和字母,
+        Random random = new Random();
+        StringBuilder saltString = new StringBuilder(length);
+        for (int i = 1; i <= length; ++i) {
+            saltString.append(ALL_CHAR_NUM.charAt(random.nextInt(ALL_CHAR_NUM.length())));
+        }
+        return saltString.toString();
+    }
+
 
     /**
      * 雪花算法 id 生成
