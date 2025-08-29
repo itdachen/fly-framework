e100f7f955b3fb4a0e48635001a9eb3b972f3bec
王大宸 <240216240@qq.com>
2025-08-29 16:29:03 +0800

    添加IP处理

---
+++ runner-spring-boot-starter/src/main/java/com/github/itdachen/boot/run/ip/IpLogConversionRule.java
@@ -0,0 +1,31 @@
+package com.github.itdachen.boot.run.ip;
+
+import ch.qos.logback.core.PropertyDefinerBase;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 保证每个 Docker 容器的日志挂载目录唯一性
+ *
+ * @author 王大宸
+ * @date 2025-08-29 16:21
+ */
+public class IpLogConversionRule extends PropertyDefinerBase {
+
+    @Override
+    public String getPropertyValue() {
+        return this.getLogIndex();
+    }
+
+    private String getLogIndex() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return String.valueOf(ThreadLocalRandom.current().nextInt(100000));
+    }
+
+}
